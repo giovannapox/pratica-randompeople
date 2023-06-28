b94e58d483ed45fc153e623a2eb28de29b8b5c43
Giovanna <giovannad1clonius@gmail.com>
2023-06-28 20:51:22 -0300

    http-status

--- index.ts
+++ index.ts
@@ -1,9 +1,8 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import router from "./src/routes/index.routes";
 
 const app = express();
 app.use(router);
-app.get("/health", (req: Request, res: Response) => res.status(200).send("I'm Ok!"));
 
 const port: number = Number(process.env.PORT) || 5000;
 app.listen(port, () => console.log(`Server is up and running on port ${port}`));
--- src/controllers/person.controller.ts
+++ src/controllers/person.controller.ts
@@ -1,14 +1,15 @@
 import { Request, Response } from "express"; 
 import { getPersonDB } from "../repositorys/person.repository";
 import { randomNumber } from "../services/randomNumber";
+import httpStatus from "http-status";
 
 export async function getPerson(req:Request, res:Response){
     try{
         const person = await getPersonDB();
         const tamanho:number = person.rowCount;
         const pessoa = randomNumber(tamanho);
-        res.status(200).send(person.rows[pessoa]);
+        res.status(httpStatus.OK).send(person.rows[pessoa]);
     } catch (err){
-        return res.status(500).send(err.message);
+        return res.status(httpStatus.INTERNAL_SERVER_ERROR).send(err.message);
     };
 };
